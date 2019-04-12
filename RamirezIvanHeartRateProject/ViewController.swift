f9c7509b114cbc1da6eba0e3df6d33661d7ea844
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2019-04-12 17:33:48 -0600

    first commit

---
+++ RamirezIvanHeartRateProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RamirezIvanHeartRateProject
+//
+//  Created by Ivan Ramirez on 4/12/19.
+//  Copyright © 2019 ramcomw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
